865ded8d19a4c714e6e52e206b92c72781569205
Zsolis-2028 <Zachary.Solis@hallmark.edu>
2026-07-21 12:23:49 -0500

    Free tier: 15 maps/month

--- src/utils/rateLimit.js
+++ src/utils/rateLimit.js
@@ -9,7 +9,7 @@
 // Free is intentionally generous during beta; tighten when billing launches.
 
 const STORAGE_KEY = 'cm_usage'
-export const MONTHLY_MAP_LIMIT = 20
+export const MONTHLY_MAP_LIMIT = 15
 
 function monthKey() {
   const d = new Date()
